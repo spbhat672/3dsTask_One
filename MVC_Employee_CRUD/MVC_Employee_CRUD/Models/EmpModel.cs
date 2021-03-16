af868a328b1d1b992072318ebcbd321b6184911d
Mr. Bhat <spbhat672@gmail.com>
2021-03-16 17:58:27 +0530

    Base _ EmpModel

---
+++ MVC_Employee_CRUD/MVC_Employee_CRUD/Models/EmpModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Employee_CRUD.Models
+{
+    public class EmpModel
+    {
+        [Display(Name="Id")]
+        public int EmpId { get; set; }
+
+        [Required(ErrorMessage ="First Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+        
+        public int Salary { get; set; }
+    }
+}
